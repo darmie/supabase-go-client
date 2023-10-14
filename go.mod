80c76d7cc5fce95924229d42dd7d297f37ea7e8d
darmie <dreakinlaja@gmail.com>
2023-10-15 00:33:35 +0100

    supabase client

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/darmie/supabase-go-client
+
+go 1.18
+
+require (
+	github.com/supabase-community/gotrue-go v1.1.0
+	github.com/supabase-community/storage-go v0.7.0
+	github.com/supabase/postgrest-go v0.0.7
+)
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/tomnomnom/linkheader v0.0.0-20180905144013-02ca5825eb80 // indirect
+)
---
+++ supabase.go
@@ -0,0 +1,35 @@
+package supabase
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/supabase-community/gotrue-go/endpoints"
+	storage_go "github.com/supabase-community/storage-go"
+	"github.com/supabase/postgrest-go"
+)
+
+type Client struct {
+	client  *http.Client
+	Auth    *endpoints.Client
+	Storage *storage_go.Client
+	DB      *postgrest.Client
+}
+
+func NewClient(projectRef string, apiKey string) (*Client, *error) {
+	dbURL, err := url.Parse(fmt.Sprintf("%s.superbase.co%s", projectRef, "/rest/v1"))
+	if err != nil {
+		panic(err)
+	}
+	storageURL, err := url.Parse(fmt.Sprintf("%s.superbase.co%s", projectRef, "/storage/v1"))
+	if err != nil {
+		panic(err)
+	}
+
+	return &Client{
+		DB:      postgrest.NewClient(dbURL.String(), "", map[string]string{"apiKey": apiKey, "Authorization": fmt.Sprintf("Bearer %s", apiKey)}),
+		Storage: storage_go.NewClient(storageURL.String(), apiKey, nil),
+		Auth:    endpoints.New(projectRef, apiKey),
+	}, nil
+}
